5917606d03c1de42ed6c9a3a75739c76d978ed04
João Victor de Souza Lucena <79459468+joaovictorsl@users.noreply.github.com>
2024-04-30 15:12:04 -0300

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
-# Uninstall conflicting packages
-echo ====\> UNINSTALL ALL CONFLICTING PACKAGES
+GREEN='\033[1;32m'
+NO_COLOR='\033[0m'
+
+echo -e "${GREEN}====> UNINSTALL ALL CONFLICTING PACKAGES${NO_COLOR}"
+
 for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
-# Set up Docker's apt repository
-echo ====\> SETTING UP DOCKER\'S APT REPOSITORY
+echo -e "${GREEN}====> SETTING UP DOCKER'S APT REPOSITORY${NO_COLOR}"
+
 sudo apt-get update &&
 sudo apt-get install ca-certificates curl -y &&
 sudo install -m 0755 -d /etc/apt/keyrings &&
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc &&
 sudo chmod a+r /etc/apt/keyrings/docker.asc &&
 
-# Add the repository to apt sources
-echo ====\> ADD REPOSITORY TO APT SOURCES
+echo -e "${GREEN}====> ADD REPOSITORY TO APT SOURCES${NO_COLOR}"
+
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 
-# Install docker
-echo ====\> INSTALLING DOCKER
+echo -e "${GREEN}====> INSTALLING DOCKER${NO_COLOR}"
+
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y &&
 
+echo -e "${GREEN}====> RUNNING POSTINSTALL STUFF${NO_COLOR}"
+
 # Configure Docker to start on boot with systemd
-echo ====\> RUNNING POSTINSTALL STUFF
 sudo systemctl enable docker.service
 sudo systemctl enable containerd.service
 
 # Manage Docker as a non-root user
+sudo groupadd docker
 sudo usermod -aG docker $USER
-newgrp docker
 
-echo ====\> DONE!
+echo -e "${GREEN}====> DONE!${NO_COLOR}"
